0c67d6012598cbe40c8eb65cd0d47bbd66e3135c
chheangg <balldremz@gmail.com>
2023-03-23 09:28:06 +0700

    Complete part 9.8 on initializing a simple endpoint

---
+++ patientor-backend/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/ping', (_req, res) => {
+  console.log('someone pinged here');
+
+  res.send('pong');
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
