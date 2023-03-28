920fcdb38b54cfaa841435b0d6dcbbb9c341952c
chheangg <balldremz@gmail.com>
2023-03-28 12:20:39 +0700

    Complete part 9.22 on adding Specific Entry type

--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -4,9 +4,46 @@ export enum Gender {
   Other = 'other'
 }
 
-export interface Entry {
+interface SickLeave {
+  startDate: string,
+  endDate: string,
 }
 
+interface Discharge {
+  date: string,
+  criteria: string
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+export enum healthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck",
+  healthCheckRating: healthCheckRating,
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare",
+  employerName: string,
+  sickLeave?: SickLeave,
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital",
+  discharge?: Discharge
+}
 
 export type Diagnose = {
   code: string,
@@ -31,5 +68,14 @@ export type NewPatient = {
   gender: Gender,
   occupation: string,
 }
+
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
